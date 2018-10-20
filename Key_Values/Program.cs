122d916db2cf8261afd57ea9924769c513a159a1
Alexander Gough <gougha3@my.erau.edu>
2018-10-20 16:40:43 -0400

    Exercise key values

--- Key_Values/Program.cs
+++ Key_Values/Program.cs
@@ -4,9 +4,43 @@ namespace Key_Values
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            MyDictionary = new MyDictionary();
+                var d = new MyDictionary();
+                try
+                {
+                    Console.WriteLine(d["Cats"]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                d["Cats"] = 42;
+                d["Dogs"] = 17;
+                Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+            
+        
         }
+    }
+
+    public class MyDictionary : KeyValue
+    {
+        int values_stored;
+        double[] numb = { 42, 17 };
+
+        string Key { get; set; }
+        object Value { get; set; }
+
+        
+    }
+    public  struct KeyValue
+    {
+        readonly
+        string key;
+        
+        readonly
+        object value;
+    
     }
 }
